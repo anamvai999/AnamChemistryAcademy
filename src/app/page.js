beb2a7f5d3ada2fb099368af46207d51b2c15c82
iamtanim0195 <sahadatmd195@gmail.com>
2024-04-21 23:24:08 +0600

    create navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import NavBar from "@/components/frontDesign/NavBar/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      ff
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <NavBar />
     </main>
   );
 }
