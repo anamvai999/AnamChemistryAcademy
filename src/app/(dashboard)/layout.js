9bf507c984488f20eeaa210c5294c19e939f348e
Md Junayed <devjunayed@gmail.com>
2024-04-22 20:55:53 +0600

    Layout and Dashboard design implementation

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -10,13 +10,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white min-h-screen p-4">
-        <div className="bg-white flex gap-4">
+      <body className="bg-white min-h-screen">
+        <div className="bg-white flex gap-4 p-4">
           <div className="w-3/12 shadow-2xl p-4 min-h-[95vh] rounded-lg ">
             <SideBarMenu />
           </div>
           <div className="w-9/12 ">
-            <div className="shadow-lg p-4 rounded-lg">
+            <div className="shadow-lg px-4 py-2 rounded-lg">
               <DashboardTopBar />
             </div>
             {children}
--- src/components/dashboard-components/DashboardTopBar/DashboardTopBar.jsx
+++ src/components/dashboard-components/DashboardTopBar/DashboardTopBar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const DashboardTopBar = () => {
   return (
-    <div>
-      Top Bar
+    <div className="flex justify-between ">
+      <input type="search" placeholder="🔎 Search Here" className="rounded-md border-black bg-white border px-2" />
+      <div className="avater w-8  ">
+        <img
+          src="/demo-avater.jpg"
+          className="border-blue-700 border-4 rounded-full"
+          alt=""
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardTopBar
+export default DashboardTopBar;
