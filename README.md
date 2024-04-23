c7490ec70c2d3e6749d0c7aa2e79f6d14e82750f
Md Junayed <devjunayed@gmail.com>
2024-04-23 18:44:53 +0600

    category Schema, model, add category design and api created

--- README.md
+++ README.md
@@ -4,10 +4,13 @@
 2. npm install @reduxjs/toolkit
 3. npm i -D daisyui@latest
 4. npm i react-icons
-
+5. npm install --save react-modal
+6. npm i mongoose
 
 ## packges 
 1. reduxjs/toolkit
 2. react-redux
 3. daisyui
 4. react-icons
+5. react-modal
+6. mongoose
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import classSlice from './features/classSlice';
+import categorySlice from './features/categorySlice';
 
 const store = configureStore({
     reducer: {
         classList: classSlice,
+        category: categorySlice
     },
 });
 
