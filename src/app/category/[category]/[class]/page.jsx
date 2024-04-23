591f1c47d79d9259b43c3090975806b9e102f3f9
iamtanim0195 <sahadatmd195@gmail.com>
2024-04-24 00:05:47 +0600

    class list

---
+++ src/app/category/[category]/[class]/page.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='flex flex-col  gap-3 justify-center items-center pt-5'>
+            <div className="card sm:card-side  w-72 sm:w-[90vw] bg-base-100 shadow-xl">
+                <figure className='sm:w-[20vw] '><img className='rounded' src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <div className="card-body ">
+                    <div className='flex justify-between'>
+                        <p>class -1</p>
+                        <p className='text-end'>10 Oct 2023</p>
+                    </div>
+                    <h2 className="card-title">
+                        মোলের সূত্র সংক্রান্ত গাণিতিক সমস্যাবলী - 2
+                    </h2>
+                    <div className="flex gap-1 ">
+                        <div className="badge badge-outline text-xs">weapon-series</div>
+                        <div className="badge badge-outline text-xs">পরিমাণগত রসায়ন</div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default page
