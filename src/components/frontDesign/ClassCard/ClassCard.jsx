be533b4758e5edfe71bcead9755dbdb56da7a773
Md Junayed <devjunayed@gmail.com>
2024-05-15 18:00:18 +0600

    video showing done

--- src/components/frontDesign/ClassCard/ClassCard.jsx
+++ src/components/frontDesign/ClassCard/ClassCard.jsx
@@ -1,35 +1,52 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-const ClassCard = ({singleClass, classNo}) => {
-    return (
-        <div className='flex flex-col  gap-3 justify-center items-center pt-5'>
-            <div className="card sm:card-side  w-72 sm:w-[90vw] bg-base-100 shadow-xl">
-                <figure className='sm:w-[20vw] '>
-                    <Image
-                        className='rounded'
-                        src={singleClass.thumbnail}
-                        alt={singleClass.title}
-                        width={500}
-                        height={500}
-                        />
-                </figure>
-                <div className="card-body ">
-                    <div className='flex justify-between'>
-                        <p>class - {classNo}</p>
-                        <p className='text-end'>{singleClass.uploadDate}</p>
-                    </div>
-                    <h2 className="card-title">
-                        {singleClass.title}
-                    </h2>
-                    <div className="flex gap-1 ">
-                        <div className="badge badge-outline text-xs">weapon-series</div>
-                        <div className="badge badge-outline text-xs">পরিমাণগত রসায়ন</div>
-                    </div>
-                </div>
-            </div>
+const ClassCard = ({ singleClass, classNo, setVideoSrc, currVideoSrc }) => {
+  const [selectedVideo, setSelectedVideo] = useState(false);
 
+  useEffect(() => {
+    if (currVideoSrc === singleClass.video) {
+      setSelectedVideo(true);
+    }else{
+        setSelectedVideo(false);
+    }
+  }, [singleClass, currVideoSrc]);
+
+  const handleClick = () => {
+    setVideoSrc(singleClass.video);
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`flex flex-col my-1 ${
+        selectedVideo ? "border border-white" : ""
+      }  gap-3 justify-center items-center pt-5`}
+    >
+      <div className="card sm:card-side   bg-base-100 shadow-xl">
+        <figure className=" ">
+          <Image
+            className="rounded"
+            src={singleClass.thumbnail}
+            alt={singleClass.title}
+            width={100}
+            height={100}
+          />
+        </figure>
+        <div className="p-4 ">
+          <div className="flex justify-between">
+            <p>class - {classNo}</p>
+            <p className="text-end">{singleClass.uploadDate}</p>
+          </div>
+          <h2 className="card-title">{singleClass.title}</h2>
+          <div className="flex gap-1 ">
+            <div className="badge badge-outline text-xs">weapon-series</div>
+            <div className="badge badge-outline text-xs">পরিমাণগত রসায়ন</div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ClassCard;
--- src/components/frontDesign/video/Video.jsx
+++ src/components/frontDesign/video/Video.jsx
@@ -14,6 +14,7 @@ const Video = ({videoSrc}) => {
      */
     const videoPlayerRef = useRef(null);
     const controlRef = useRef(null);
+    const playerContainerRef = useRef(null);
 
     const [videoState, setVideoState] = useState({
         playing: true,
@@ -124,27 +125,31 @@ const Video = ({videoSrc}) => {
         setVideoState({ ...videoState, buffer: false });
     };
 
+    const handleFullScreen = () => {
+        if (playerContainerRef.current) {
+            if (document.fullscreenElement) {
+                document.exitFullscreen();
+            } else {
+                playerContainerRef.current.requestFullscreen();
+            }
+        }
+    };
     return (
         <div className="video_container">
-            <div>
-                <h2>React player</h2>
-            </div>
             <Container maxWidth="md" justify="center">
-                <div className="player__wrapper" onMouseMove={mouseMoveHandler}>
+                <div ref={playerContainerRef} className="player__wrapper w-full min-h-[70vh]" onMouseMove={mouseMoveHandler}>
                     <ReactPlayer
-                        
                         ref={videoPlayerRef}
-                        className="player"
+                        className="player min-h-[70vh]"
                         url={videoSrc}
                         width="100%"
-                        height="100%"
+                        height="50vh"
                         playing={playing}
                         volume={volume}
                         muted={muted}
                         onProgress={progressHandler}
                         onBuffer={bufferStartHandler}
                         onBufferEnd={bufferEndHandler}
-                        
                     />
 
                     {buffer && <p>Loading</p>}
@@ -170,6 +175,7 @@ const Video = ({videoSrc}) => {
                     />
                 </div>
             </Container>
+            <button onClick={handleFullScreen}>Full Screen</button>
         </div>
     )
 }
