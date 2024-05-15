ed4bea161a784e42cd0b3eb35d9500a65441b245
Md Junayed <devjunayed@gmail.com>
2024-05-15 20:09:19 +0600

    video player mobile problem fix test-1

--- src/components/frontDesign/video/Video.jsx
+++ src/components/frontDesign/video/Video.jsx
@@ -1,183 +1,197 @@
-"use client"
+"use client";
 
-import { useRef, useState } from 'react';
-import './Video.css';
-import dynamic from 'next/dynamic'
-import Control from './Control';
-import { Container } from '@mui/material';
-import { formatTime } from './Format';
-import ReactPlayer from 'react-player';
+import { useRef, useState } from "react";
+import "./Video.css";
+import dynamic from "next/dynamic";
+import Control from "./Control";
+import { Container } from "@mui/material";
+import { formatTime } from "./Format";
+import ReactPlayer from "react-player";
 
 let count = 0;
-const Video = ({videoSrc}) => {
-    /*     const ReactPlayer = dynamic(() => import("react-player/youtube"), { ssr: false });
-     */
-    const videoPlayerRef = useRef(null);
-    const controlRef = useRef(null);
-    const playerContainerRef = useRef(null);
+const Video = ({ videoSrc }) => {
+  /*     const ReactPlayer = dynamic(() => import("react-player/youtube"), { ssr: false });
+   */
+  const videoPlayerRef = useRef(null);
+  const controlRef = useRef(null);
+  const playerContainerRef = useRef(null);
 
-    const [videoState, setVideoState] = useState({
-        playing: true,
-        muted: false,
-        volume: 0.5,
-        playbackRate: 1.0,
-        played: 0,
-        seeking: false,
-        buffer: true,
+  const [videoState, setVideoState] = useState({
+    playing: true,
+    muted: false,
+    volume: 0.5,
+    playbackRate: 1.0,
+    played: 0,
+    seeking: false,
+    buffer: true,
+  });
+
+  //Destructuring the properties from the videoState
+  const { playing, muted, volume, playbackRate, played, seeking, buffer } =
+    videoState;
+
+  const currentTime = videoPlayerRef.current
+    ? videoPlayerRef.current.getCurrentTime()
+    : "00:00";
+  const duration = videoPlayerRef.current
+    ? videoPlayerRef.current.getDuration()
+    : "00:00";
+
+  const formatCurrentTime = formatTime(currentTime);
+  const formatDuration = formatTime(duration);
+
+  const playPauseHandler = () => {
+    //plays and pause the video (toggling)
+    setVideoState({ ...videoState, playing: !videoState.playing });
+  };
+
+  const rewindHandler = () => {
+    //Rewinds the video player reducing 5
+    videoPlayerRef.current.seekTo(videoPlayerRef.current.getCurrentTime() - 5);
+  };
+
+  const handleFastFoward = () => {
+    //FastFowards the video player by adding 10
+    videoPlayerRef.current.seekTo(videoPlayerRef.current.getCurrentTime() + 10);
+  };
+
+  //console.log("========", (controlRef.current.style.visibility = "false"));
+  const progressHandler = (state) => {
+    if (count > 3) {
+      console.log("close");
+      controlRef.current.style.visibility = "hidden"; // toggling player control container
+    } else if (controlRef.current.style.visibility === "visible") {
+      count += 1;
+    }
+
+    if (!seeking) {
+      setVideoState({ ...videoState, ...state });
+    }
+  };
+
+  const seekHandler = (e, value) => {
+    setVideoState({ ...videoState, played: parseFloat(value / 100) });
+    videoPlayerRef.current.seekTo(parseFloat(value / 100));
+  };
+
+  const seekMouseUpHandler = (e, value) => {
+    console.log(value);
+
+    setVideoState({ ...videoState, seeking: false });
+    videoPlayerRef.current.seekTo(value / 100);
+  };
+
+  const volumeChangeHandler = (e, value) => {
+    const newVolume = parseFloat(value) / 100;
+
+    setVideoState({
+      ...videoState,
+      volume: newVolume,
+      muted: Number(newVolume) === 0 ? true : false, // volume === 0 then muted
     });
+  };
 
-    //Destructuring the properties from the videoState
-    const { playing, muted, volume, playbackRate, played, seeking, buffer } =
-        videoState;
+  const volumeSeekUpHandler = (e, value) => {
+    const newVolume = parseFloat(value) / 100;
 
-    const currentTime = videoPlayerRef.current
-        ? videoPlayerRef.current.getCurrentTime()
-        : "00:00";
-    const duration = videoPlayerRef.current
-        ? videoPlayerRef.current.getDuration()
-        : "00:00";
+    setVideoState({
+      ...videoState,
+      volume: newVolume,
+      muted: newVolume === 0 ? true : false,
+    });
+  };
 
-    const formatCurrentTime = formatTime(currentTime);
-    const formatDuration = formatTime(duration);
+  const muteHandler = () => {
+    //Mutes the video player
+    setVideoState({ ...videoState, muted: !videoState.muted });
+  };
 
-    const playPauseHandler = () => {
-        //plays and pause the video (toggling)
-        setVideoState({ ...videoState, playing: !videoState.playing });
-    };
+  const onSeekMouseDownHandler = (e) => {
+    setVideoState({ ...videoState, seeking: true });
+  };
 
-    const rewindHandler = () => {
-        //Rewinds the video player reducing 5
-        videoPlayerRef.current.seekTo(videoPlayerRef.current.getCurrentTime() - 5);
-    };
+  const mouseMoveHandler = () => {
+    controlRef.current.style.visibility = "visible";
+    count = 0;
+  };
 
-    const handleFastFoward = () => {
-        //FastFowards the video player by adding 10
-        videoPlayerRef.current.seekTo(videoPlayerRef.current.getCurrentTime() + 10);
-    };
+  const bufferStartHandler = () => {
+    console.log("Bufering.......");
+    setVideoState({ ...videoState, buffer: true });
+  };
 
-    //console.log("========", (controlRef.current.style.visibility = "false"));
-    const progressHandler = (state) => {
-        if (count > 3) {
-            console.log("close");
-            controlRef.current.style.visibility = "hidden"; // toggling player control container
-        } else if (controlRef.current.style.visibility === "visible") {
-            count += 1;
-        }
+  const bufferEndHandler = () => {
+    console.log("buffering stoped ,,,,,,play");
+    setVideoState({ ...videoState, buffer: false });
+  };
 
-        if (!seeking) {
-            setVideoState({ ...videoState, ...state });
-        }
-    };
+  const handleFullScreen = () => {
+    if (playerContainerRef.current) {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      } else {
+        playerContainerRef.current.requestFullscreen();
+      }
+    }
+  };
 
-    const seekHandler = (e, value) => {
-        setVideoState({ ...videoState, played: parseFloat(value / 100) });
-        videoPlayerRef.current.seekTo(parseFloat(value / 100));
-    };
+  const handleTouch = () => {
+    controlRef.current.style.visibility = "visible";
+    count = 0;
+    // alert("bang bang");
 
-    const seekMouseUpHandler = (e, value) => {
-        console.log(value);
+  };
 
-        setVideoState({ ...videoState, seeking: false });
-        videoPlayerRef.current.seekTo(value / 100);
-    };
+  return (
+    <div className="video_container" onTouchStart={handleTouch}>
+      <Container maxWidth="md" justify="center">
+        <div
+          ref={playerContainerRef}
+          className="player__wrapper w-full min-h-[70vh] z-20"
+          onTouchStart={handleTouch}
+          onMouseMove={mouseMoveHandler}
+        >
+          <ReactPlayer
+            onTouchStart={handleTouch}
+            ref={videoPlayerRef}
+            className="player min-h-[70vh] z-0"
+            url={videoSrc}
+            width="100%"
+            height="50vh"
+            playing={playing}
+            volume={volume}
+            muted={muted}
+            onProgress={progressHandler}
+            onBuffer={bufferStartHandler}
+            onBufferEnd={bufferEndHandler}
+          />
 
-    const volumeChangeHandler = (e, value) => {
-        const newVolume = parseFloat(value) / 100;
+          {buffer && <p>Loading</p>}
 
-        setVideoState({
-            ...videoState,
-            volume: newVolume,
-            muted: Number(newVolume) === 0 ? true : false, // volume === 0 then muted
-        });
-    };
-
-    const volumeSeekUpHandler = (e, value) => {
-        const newVolume = parseFloat(value) / 100;
-
-        setVideoState({
-            ...videoState,
-            volume: newVolume,
-            muted: newVolume === 0 ? true : false,
-        });
-    };
-
-    const muteHandler = () => {
-        //Mutes the video player
-        setVideoState({ ...videoState, muted: !videoState.muted });
-    };
-
-    const onSeekMouseDownHandler = (e) => {
-        setVideoState({ ...videoState, seeking: true });
-    };
-
-    const mouseMoveHandler = () => {
-        controlRef.current.style.visibility = "visible";
-        count = 0;
-    };
-
-    const bufferStartHandler = () => {
-        console.log("Bufering.......");
-        setVideoState({ ...videoState, buffer: true });
-    };
-
-    const bufferEndHandler = () => {
-        console.log("buffering stoped ,,,,,,play");
-        setVideoState({ ...videoState, buffer: false });
-    };
-
-    const handleFullScreen = () => {
-        if (playerContainerRef.current) {
-            if (document.fullscreenElement) {
-                document.exitFullscreen();
-            } else {
-                playerContainerRef.current.requestFullscreen();
-            }
-        }
-    };
-    return (
-        <div className="video_container">
-            <Container maxWidth="md" justify="center">
-                <div ref={playerContainerRef} className="player__wrapper w-full min-h-[70vh]" onMouseMove={mouseMoveHandler}>
-                    <ReactPlayer
-                        ref={videoPlayerRef}
-                        className="player min-h-[70vh]"
-                        url={videoSrc}
-                        width="100%"
-                        height="50vh"
-                        playing={playing}
-                        volume={volume}
-                        muted={muted}
-                        onProgress={progressHandler}
-                        onBuffer={bufferStartHandler}
-                        onBufferEnd={bufferEndHandler}
-                    />
-
-                    {buffer && <p>Loading</p>}
-
-                    <Control
-                        controlRef={controlRef}
-                        onPlayPause={playPauseHandler}
-                        playing={playing}
-                        onRewind={rewindHandler}
-                        onForward={handleFastFoward}
-                        played={played}
-                        onSeek={seekHandler}
-                        onSeekMouseUp={seekMouseUpHandler}
-                        volume={volume}
-                        onVolumeChangeHandler={volumeChangeHandler}
-                        onVolumeSeekUp={volumeSeekUpHandler}
-                        mute={muted}
-                        onMute={muteHandler}
-                        playRate={playbackRate}
-                        duration={formatDuration}
-                        currentTime={formatCurrentTime}
-                        onMouseSeekDown={onSeekMouseDownHandler}
-                    />
-                </div>
-            </Container>
-            <button onClick={handleFullScreen}>Full Screen</button>
+          <Control
+            controlRef={controlRef}
+            onPlayPause={playPauseHandler}
+            playing={playing}
+            onRewind={rewindHandler}
+            onForward={handleFastFoward}
+            played={played}
+            onSeek={seekHandler}
+            onSeekMouseUp={seekMouseUpHandler}
+            volume={volume}
+            onVolumeChangeHandler={volumeChangeHandler}
+            onVolumeSeekUp={volumeSeekUpHandler}
+            mute={muted}
+            onMute={muteHandler}
+            playRate={playbackRate}
+            duration={formatDuration}
+            currentTime={formatCurrentTime}
+            onMouseSeekDown={onSeekMouseDownHandler}
+          />
         </div>
-    )
-}
+      </Container>
+      <button onClick={handleFullScreen}>Full Screen</button>
+    </div>
+  );
+};
 
 export default Video;
