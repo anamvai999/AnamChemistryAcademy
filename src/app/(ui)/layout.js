dbcce40348461ea1fbaf412f62468c6bfee76931
iamtanim0195 <sahadatmd195@gmail.com>
2024-07-11 23:20:09 +0600

    add title

--- src/app/(ui)/layout.js
+++ src/app/(ui)/layout.js
@@ -7,7 +7,7 @@ import { AuthProvider } from "@/context/authContext/AuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Anam Chemistry Academy",
+  title: process.env.NEXT_PUBLIC_TITLE,
   description:
     "Anam Chemistry Academy is developed by Md Shadat Hossain Tanim and Md Junayed",
 };
