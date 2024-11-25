2cfdec0ef7d64fb4dc4872c8f80afd638f07e9ab
iamtanim0195 <sahadatmd195@gmail.com>
2024-11-26 05:23:31 +0600

    <D:/apk/Git/IsAuthorized> add on exam

--- src/app/(ui)/exams/page.jsx
+++ src/app/(ui)/exams/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import IsAdmin from "@/components/common/IsAdmin";
+import IsAuthorized from "@/components/common/IsAuthorized";
 import AddExam from "@/components/frontDesign/AddExam/AddExam";
 import React from "react";
 import useSWR from "swr";
@@ -35,29 +36,31 @@ const Page = () => {
       <IsAdmin>
         <AddExam refetch={mutate} />
       </IsAdmin>
-      <div className="mt-10 flex gap-4 items-center flex-col mx-8 md:mx-30 lg:mx-36">
-        {exams?.length !== 0 &&
-          exams?.map((exam, index) => (
-            <div key={exam._id} className="flex w-full gap-4">
-              <Link
-                href={`/exams/${exam._id}`}
-                className="flex gap-2 shadow-sm border-zinc-600 px-6 border-s-4 rounded-s-xl border-e-4 border-e-zinc-600  py-3 w-full items-center"
-              >
-                {index + 1}.
-                <Exam {...exam} />
-              </Link>
-              <IsAdmin>
-                <button
-                  onClick={() => { }}
-                  className="flex items-center justify-center gap-2 rounded px-3 py-2 bg-green-500 text-white"
+      <IsAuthorized>
+        <div className="mt-10 flex gap-4 items-center flex-col mx-8 md:mx-30 lg:mx-36">
+          {exams?.length !== 0 &&
+            exams?.map((exam, index) => (
+              <div key={exam._id} className="flex w-full gap-4">
+                <Link
+                  href={`/exams/${exam._id}`}
+                  className="flex gap-2 shadow-sm border-zinc-600 px-6 border-s-4 rounded-s-xl border-e-4 border-e-zinc-600  py-3 w-full items-center"
                 >
-                  <BiEdit /> Edit
-                </button>
-                <EditExam refetch={mutate} data={exam} />
-              </IsAdmin>
-            </div>
-          ))}
-      </div>
+                  {index + 1}.
+                  <Exam {...exam} />
+                </Link>
+                <IsAdmin>
+                  <button
+                    onClick={() => { }}
+                    className="flex items-center justify-center gap-2 rounded px-3 py-2 bg-green-500 text-white"
+                  >
+                    <BiEdit /> Edit
+                  </button>
+                  <EditExam refetch={mutate} data={exam} />
+                </IsAdmin>
+              </div>
+            ))}
+        </div>
+      </IsAuthorized>
     </main>
   );
 };
